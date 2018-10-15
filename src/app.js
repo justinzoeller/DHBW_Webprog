d81b3cdd8d13f56fdbab3e891cde1c81a49b0e3f
Justin <Justin@Justins-MacBook-Air.local>
2018-10-15 14:28:23 +0200

    JavaScript angelegt.

---
+++ src/app.js
@@ -0,0 +1,23 @@
+// Einlesen der Daten aus der Eingabe für Aktivität //
+
+let nameakt = document.getElementById(nameakt).value;
+let zeitbis = document.getElementById(zeitbis).value;
+let zeitvon = document.getElementById(zeitbis).value;
+
+// Abschicken des Formulars
+
+let abschicken = event => {
+  let nameakt = document.getElementById(nameakt).value;
+  let zeitbis = document.getElementById(zeitbis).value;
+  let zeitvon = document.getElementById(zeitbis).value;
+  let okay = true;
+  let message = "";
+
+  // Überprüfung des Eingabefelds
+   if (nameakt == "") {
+     okay = false;
+     message = "Hey, du hast vergessen, deiner Aktivität einen Namen zu geben!";
+     
+   }
+}
+}
