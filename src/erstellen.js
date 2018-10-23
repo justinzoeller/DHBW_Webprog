a0e32d4de4710c5f694ea5feab57de5b8f419c9c
Justin <Justin@Justins-MacBook-Air.local>
2018-10-23 14:12:08 +0200

    Firebase eingebunden.

--- src/erstellen.js
+++ src/erstellen.js
@@ -1,42 +1,43 @@
 // Abrufen des Formulars
 
-let name = document.getElementById("name").value;
-let tag = document.getElementById("tag").value;
-let zeitvon = document.getElementById("zeitvon").value;
-let zeitbis = document.getElementById("zeitbis").value;
-let prio = document.getElementById("prio").value;
-let priooption = prio.selectedOptions;
-console.log(priooption)
+
+
+  // Initialize Fi
+
+
+function einreichen(event) {
+  alert("Dein Tag wurde erfolgreich in die Liste aufgenommen");
+    name
+
+  var postData = {
+    name : document.getElementById("name").value,
+    tag : document.getElementById("tag").value,
+    prio : document.getElementById("prio").value,
+    zeitbis : document.getElementById("zeitbis").value,
+    zeitvon : document.getElementById("zeitvon").value,
+  };
+
+var newPostKey = firebase.database().ref().child('posts').push().key;
+var updates = {};
+ updates['/posts/' + newPostKey] = postData;
+ updates['/user-posts/'+ newPostKey] = postData;
+
+ return firebase.database().ref().update(updates);
+}
+
+
 
 // Zuordnung der Angaben in das richtige Eingabefeld
 
 // Anordnung anhand von Tag, Uhrzeit und Name
 
-function einordnen (name, tag, zeitvon, zeitbis, prio) {
-  if (tag=="Montag" && zeitvon=="8:00 Uhr" && zeitbis=="9:00 Uhr") {
-    document.getElementById("mo8-9").innerHTML = name;
-  }
-  window.onload = function () {
-	document.getElementById("einreichen")
-		.onclick = einordnen;
+
+window.onload = function () {
+  console.log("Dokumente geladen");
+  document.getElementById("ein").addEventListener("click",einreichen(event));
 }
+
 
 // Die vom Formular erfassten Daten an Google Firebase weiterleiten
 
-function saveProject() {
-
-  var PostData = {
-  name: document.getElementById("name").value;
-  tag: document.getElementById("tag").value;
-  zeitvon: document.getElementById("zeitvon").value;
-  zeitbis: document.getElementById("zeitbis").value;
-  prio: document.getElementById("prio").value;
-}
-
-var newPostKey = firebase.database().ref().child('posts').push().key;
-
-var updates = {};
-updates['/posts/' + newPostKey] = postData;
-updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-
-return firebase.database().ref().update(updates);
+//function saveProject() {
