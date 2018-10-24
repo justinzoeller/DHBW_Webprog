40210483a3097aa020675d027ea649b0ca13eef7
Justin <Justin@Justins-MacBook-Air.local>
2018-10-24 15:27:14 +0200

    Zuordnungsskript angefangen

---
+++ src/zuordnung.js
@@ -0,0 +1,21 @@
+// Referenz auf Google firebase
+
+var database = firebase.database();
+
+window.onload = zuordnen () {
+
+  // aus der Datenbank auslesen
+  for (i in webprog) {
+
+    // jetzt folgen die Abfragen für die Zuordnung
+
+    // Montag, 8-9
+
+    if (webprog[i].tag == "Montag" && webprog[i].zeitvon == "8:00 Uhr" && webprog[i].zeitbis="9:00 Uhr") {
+
+      // Prioritätsprüfung & - darstellung
+
+      if (webprog[i].prio == "keine Priorität")
+    }
+  }
+}
