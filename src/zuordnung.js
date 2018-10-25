274445088bc134546e186568adeca4f6b4688759
Justin <Justin@Justins-MacBook-Air.local>
2018-10-25 16:31:47 +0200

    Zuordnungsalgorithmus verbessert.

--- src/zuordnung.js
+++ src/zuordnung.js
@@ -4,6 +4,17 @@
 
 window.onload = function () {
 
+  // Für die einzelnen Tages- und Zeitkombinationen müssen Parent-Elemente erzeugt werden
+
+  var mo1 = document.createElement("li");
+  var mo2 = document.createElement("li");
+  var mo3 = document.createElement("li");
+
+  // Radiobuttons definieren
+
+  var radio = document.createElement("input");
+  radio.setAttribute("type", "radio");
+
   // Firebase einbinden
   var database = firebase.database();
   var rootRef = firebase.database().ref("posts");
@@ -14,35 +25,33 @@ window.onload = function () {
   // aus der Datenbank auslesen
   for (i in webprog){
 
-    // jetzt folgen die Abfragen für die Zuordnung
-
-    // Montag, 8-9
-
-
-
     if (webprog[i].tag == "Montag" && webprog[i].zeitvon == "8:00 Uhr" && webprog[i].zeitbis =="9:00 Uhr") {
-
-      // Prioritätsprüfung & - darstellung
 
       if (webprog[i].prio == "keine Priorität") {
         // Wenn keine Priorität, keine zusätzliche Zeichen
-        document.getElementById("mo8-9").innerHTML = webprog[i].name;
+        var mo1p1 = document.createTextNode(webprog[i].name + "\n");
+        mo1.appendChild(mo1p1);
       }
 
       if (webprog[i].prio == "wenig Priorität") {
         // Wenn wenig Priorität, nur ein Ausrufezeichen
-        document.getElementById("mo8-9").innerHTML = webprog[i].name + " " + "!";
+        var mo1p2 = document.createTextNode(webprog[i].name + "" + "!\n" );
+        mo1.appendChild(mo1p2);
       }
 
       if (webprog[i].prio == "mittlere Priorität") {
         // Wenn mittlere Priorität, dann zwei Ausrufezeichen
-        document.getElementById("mo8-9").innerHTML = webprog[i].name + " " + "!!";
+        var mo1p3 = document.createTextNode(webprog[i].name + "" + "!!\n");
+        mo1.appendChild(mo1p3);
       }
 
       if (webprog[i].prio == "hohe Priorität") {
         // Wenn hohe Priorität, dann drei Ausrufezeichen
-        document.getElementById("mo8-9").innerHTML = webprog[i].name + " " + "!!!";
+        var mo1p4 = document.createTextNode(webprog[i].name + "" + "!!!\n");
+        mo1.appendChild(mo1p4);
       }
+
+      document.getElementById("mo8-9").appendChild(mo1);
 
     }
   }
