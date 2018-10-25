f158efcf4d1167b6c8bea1f8589fa4c8bfadff61
Justin <Justin@Justins-MacBook-Air.local>
2018-10-25 17:30:12 +0200

    Zuordnungsalgorithmus weitergearbeitet.

--- src/zuordnung.js
+++ src/zuordnung.js
@@ -6,14 +6,11 @@ window.onload = function () {
 
   // Für die einzelnen Tages- und Zeitkombinationen müssen Parent-Elemente erzeugt werden
 
-  var mo1 = document.createElement("li");
+  var mo1 = document.createElement("mo1");
   var mo2 = document.createElement("li");
   var mo3 = document.createElement("li");
 
-  // Radiobuttons definieren
 
-  var radio = document.createElement("input");
-  radio.setAttribute("type", "radio");
 
   // Firebase einbinden
   var database = firebase.database();
@@ -29,25 +26,25 @@ window.onload = function () {
 
       if (webprog[i].prio == "keine Priorität") {
         // Wenn keine Priorität, keine zusätzliche Zeichen
-        var mo1p1 = document.createTextNode(webprog[i].name + "\n");
+        var mo1p1 = document.createTextNode(webprog[i].name + "\r\n");
         mo1.appendChild(mo1p1);
       }
 
       if (webprog[i].prio == "wenig Priorität") {
         // Wenn wenig Priorität, nur ein Ausrufezeichen
-        var mo1p2 = document.createTextNode(webprog[i].name + "" + "!\n" );
+        var mo1p2 = document.createTextNode(webprog[i].name + "" + "!\r\n" );
         mo1.appendChild(mo1p2);
       }
 
       if (webprog[i].prio == "mittlere Priorität") {
         // Wenn mittlere Priorität, dann zwei Ausrufezeichen
-        var mo1p3 = document.createTextNode(webprog[i].name + "" + "!!\n");
+        var mo1p3 = document.createTextNode(webprog[i].name + "" + "!!\r\n");
         mo1.appendChild(mo1p3);
       }
 
       if (webprog[i].prio == "hohe Priorität") {
         // Wenn hohe Priorität, dann drei Ausrufezeichen
-        var mo1p4 = document.createTextNode(webprog[i].name + "" + "!!!\n");
+        var mo1p4 = document.createTextNode(webprog[i].name + "" + "!!!\r\n");
         mo1.appendChild(mo1p4);
       }
 
