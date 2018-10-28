0b3e789ffa6bb8e8ca86011e1f452b6e85f723c1
melakc <38559247+melakc@users.noreply.github.com>
2018-10-29 00:25:36 +0100

    Searchbar funktion

--- src/suchen.js
+++ src/suchen.js
@@ -1,34 +1,55 @@
+window.addEventListener("load",() =>{
+ 
+         
+          let database = firebase.database();
+          database.ref('posts').orderByChild('name').on("value", function(snapshot) {
+            snapshot.forEach(function(data) {
+              key = data.key;
+              name2 = data.val().name;
+              if ('Neue Aktivität' == name2) {
 
-window.onload=searchFunction(){
+              }
+        
+            });
+          });
+});
 
-  // Firebase einbinden
-  var database = firebase.database();
-  var rootRef = firebase.database().ref("posts");
-  rootRef.once("value").then(function(snapshot) {
-  var anzahlChild = snapshot.numChildren();
-  var webprog = snapshot.val();
+function searchFunction () {
+    let database = firebase.database();
+    database.ref('posts').orderByChild('name').on("value", function(snapshot) {
+        let seachvalue = document.getElementById('search').value;
+        let webprog = snapshot.val();
+      
+        let message = '';
+        let gefunden = false;
 
-//   let name = document.getElementById("name").value;
-  let name=document.getElementById("name");
-  let search=document.getElementById("search").value;
-  document.getElementById("name").addEventListener('keydown',function(event){
-      if(event.which==10){
-        searching()
-      }
-  })
-  search.addEventListener("click",()=>{
-      searching()
-  })
-  function searching(){
-     for(i in webprog){
-    if(webprog[i].name!=null){
+        
 
-    
-    return firebase.database().ref().
+        for (i in webprog){ 
+            let searchtocompare = seachvalue.replace(/\s*$/,'');
+            nametocompare = webprog[i].name;
+            nametocompare = nametocompare.replace(/\s*$/,'');
+            searchtocompare = searchtocompare.toUpperCase();
+            nametocompare = nametocompare.toUpperCase();
+            if (searchtocompare == nametocompare) {
+            gefunden = true;
+            message = message + 'Tag: ' + webprog[i].tag + ' \n';
+            message = message + 'Ab : ' + webprog[i].zeitvon + ' \n';
+            message = message + 'Bis: ' + webprog[i].zeitbis + ' \n';
+            message = message + 'Prio: ' + webprog[i].prio + ' \n';
+            message = message + '--------------------------- \n';
+            }
+        }
 
-    } 
-  }
-  
-  }
-    
+        if(gefunden == true) {  
+           message = 'Die Aktivität ' + seachvalue + ' ist eingetragen am: \n ' + message;
+        } else {
+            message = seachvalue + ' ist keine eingetragene Aktivität!';
+        }
+
+        window.alert(message);
+    });
 }
+
+        
+     
